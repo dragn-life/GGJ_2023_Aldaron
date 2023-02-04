f5346f4d8bf6c0fce97f55db62429614f8c56df7
Adam Bednarek <adamjb51@gmail.com>
2023-02-03 20:32:16 -0500

    add effects
    * Spawn Effect
    * Death effect
    * Lower volumes

--- Assets/_Scripts/States/Enemy/EnemyDeathState.cs
+++ Assets/_Scripts/States/Enemy/EnemyDeathState.cs
@@ -6,13 +6,14 @@ namespace _Scripts.States.Enemy
   {
     public override void EnterState(EnemyController enemy)
     {
+      float deathTime = 5.0f;
       // Debug.Log("Enter Death State");
       enemy.navMeshAgent.speed = 0.0f;
       enemy.Animator.SetBool("Walk Forward", false);
       enemy.Animator.SetTrigger("Die");
       enemy.FreezeAnimation(1.3f);
-      enemy.PlayDeathEffects();
-      enemy.DestroySelf(5f);
+      enemy.PlayDeathEffects(deathTime);
+      enemy.DestroySelf(deathTime);
     }
 
     public override void OnUpdate(EnemyController enemy)
